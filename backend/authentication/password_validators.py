359384554fe59261b4eddd890d3121e7f60263e9
pbirl <metlifelegalpla\pooja@a-no011yjxlq92.metlifelegalplans.legalplans.com>
2020-11-19 17:03:05 -0500

    BACKEND: Password strength on backend should be restricted to a reasonably good score

--- backend/authentication/password_validators.py
+++ backend/authentication/password_validators.py
@@ -2,10 +2,12 @@ import re
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
+from zxcvbn import zxcvbn
 
 
 has_alphabetic = re.compile(r"[a-zA-Z]")
 has_numeric = re.compile(r"[\d]")
+MIN_SCORE = 3
 
 
 class AlphaNumericPasswordValidator:
@@ -23,4 +25,21 @@ class AlphaNumericPasswordValidator:
         return _(
             "Your password must contain at least 1 numeric character and 1 alphabetic "
             "character"
+        )
+
+
+class PasswordStrengthValidator:
+    def validate(self, password, user=None):
+        help_string = "Your password needs to be strong." \
+                      "Try making it longer or adding in numeric and special characters to make it stronger."
+        score_result = zxcvbn(password)
+        keys_values = score_result.get('score')
+        if keys_values < MIN_SCORE:
+            raise ValidationError(
+                self.help_string
+            )
+
+    def get_help_text(self):
+        return _(
+            self.help_string
         )
